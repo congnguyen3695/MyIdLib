6bbf023c54c5089176ab20382a5223352833d376
congnguyen <nguyenmanhcong1995@gmail.com>
2024-02-26 11:47:28 +0700

    Initial Commit

---
+++ MyIdLib/MyIdLib.h
@@ -0,0 +1,18 @@
+//
+//  MyIdLib.h
+//  MyIdLib
+//
+//  Created by congnguyen on 26/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyIdLib.
+FOUNDATION_EXPORT double MyIdLibVersionNumber;
+
+//! Project version string for MyIdLib.
+FOUNDATION_EXPORT const unsigned char MyIdLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyIdLib/PublicHeader.h>
+
+
